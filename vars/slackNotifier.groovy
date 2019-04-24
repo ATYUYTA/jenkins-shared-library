08e9992c876d262dbed11fde29b513182ba52f00
Chester Cheng <Chester.Cheng@redcypress.com.tw>
2019-04-24 10:53:27 +0800

    update library

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,7 +2,7 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} was successful(${env.BUILD_URL})"
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
